0ee951d061719498f185340e30933560ca603683
iNaneek <Keenan.manchester@gmail.com>
2024-04-08 22:13:30 -0700

    day 1

---
+++ day 3/terrible-encrypter.py
@@ -0,0 +1,8 @@
+import random  # uncrackable encrypting, even with a key
+inp = list(input("Text to encrypt: "))
+
+enc = ''
+for i in range(len(inp)):
+    enc += chr(random.randint(0,66))
+
+print(enc)
